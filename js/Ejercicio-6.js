5fd4a36656c5b92e35c966786da07fca0a02e05d
Sofi Delgado <sofi5_09@hotmail.com>
2023-02-27 22:53:24 -0300

    Ejercicio 6

---
+++ js/Ejercicio-6.js
@@ -0,0 +1,10 @@
+let numero1 = window.prompt("Escriba numero 1");
+let numero2 = window.prompt("Escriba numero 2");
+if(numero1>numero2)
+{
+    alert("El mayor es: "+numero1);
+}
+else
+{
+    alert("El mayor es: " + numero2)
+}
